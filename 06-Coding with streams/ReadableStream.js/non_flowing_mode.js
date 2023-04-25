54ed8df2fe9cdeca424d3351b211e2205b33a433
Rohan Tiwari <ping.techyrohan@gmail.com>
2023-04-25 20:48:32 +0545

    typo fix

--- 06-Coding with streams/ReadableStream.js/non_flowing_mode.js
+++ 06-Coding with streams/ReadableStream.js/non_flowing_mode.js
@@ -3,6 +3,9 @@
 
 process.stdin.on("readable", () => {
   let chunk;
+
+  // here we use while loop to make continious read
+  // flowing mode doesnot need loop
   while ((chunk = process.stdin.read()) !== null) {
     console.log(`You entered ${chunk.toString()}`);
   }
