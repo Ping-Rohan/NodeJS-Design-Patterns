dfad9e6ed8f6c4ad58b3d99e7257835bae5d090d
Rohan Tiwari <ping.techyrohan@gmail.com>
2023-04-29 00:44:07 +0545

    Proxy Example

--- 08-Structural Design Patterns/Proxy.js
+++ 08-Structural Design Patterns/Proxy.js
@@ -22,3 +22,95 @@
     Remote objects: The proxy can take a remote object and make it appear local
 
 */
+
+// SIMPLE STACK CALCULATOR
+
+class StackCalculator {
+  constructor() {
+    this.stack = [];
+  }
+
+  putValue(value) {
+    return this.stack.push(value);
+  }
+
+  getValue() {
+    return this.stack.pop();
+  }
+
+  peekValue() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  clear() {
+    this.stack = [];
+  }
+
+  divide() {
+    const divisor = this.getValue();
+    const dividend = this.getValue();
+    const result = dividend / divisor;
+    this.putValue(result);
+    return result;
+  }
+
+  multiply() {
+    const multiplicand = this.getValue();
+    const multiplier = this.getValue();
+    const result = multiplier * multiplicand;
+    this.putValue(result);
+    return result;
+  }
+}
+
+// HERE IS HOW WE CAN USE STACK CALCULATOR CLASS
+
+const calculator = new StackCalculator();
+
+// NOW LETS IMPLEMENT SAME CALCULATOR USING PROXY PATTERN OR OBJECT COMPOSITION
+
+class SafeCalculator {
+  constructor(calculator) {
+    this.calculator = calculator;
+  }
+
+  divide() {
+    const divisor = this.calculator.peekValue();
+    if (divisor === 0) {
+      throw new Error("Divisor is 0");
+    }
+    return this.calculator.divide();
+  }
+
+  // DELEGATED METHODS OR AUTHORIZED METHODS FROM ANOTHER CLASS
+
+  putValue(value) {
+    return this.calculator.putValue(value);
+  }
+
+  getValue() {
+    return this.calculator.getValue();
+  }
+
+  peekValue() {
+    return this.calculator.peekValue();
+  }
+
+  clear() {
+    return this.calculator.clear();
+  }
+
+  multiply() {
+    return this.calculator.multiply();
+  }
+}
+
+const safeCalculation = new SafeCalculator(calculator);
+
+safeCalculation.putValue(4);
+safeCalculation.putValue(0);
+
+// NOW THIS THROWS AN ERROR
+// OUR ACCESS WONT GO UPTO CALCULATOR BECAUSE PROXY WILL REJECT THE REQUEST
+
+console.log(safeCalculation.divide());
